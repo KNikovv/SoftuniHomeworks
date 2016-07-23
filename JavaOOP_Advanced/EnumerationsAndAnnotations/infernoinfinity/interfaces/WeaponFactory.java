58998e81a55415b419578fa8b84574859a106357
KNikovv <kaloyannikov@gmail.com>
2016-07-23 14:24:44 +0300

    a

--- JavaOOP_Advanced/EnumerationsAndAnnotations/infernoinfinity/interfaces/WeaponFactory.java
+++
@@ -1,9 +0,0 @@
-package infernoinfinity.interfaces;
-
-import infernoinfinity.enums.WeaponType;
-
-public interface WeaponFactory {
-
-    Weapon forgeWeapon(String weaponName, WeaponType weaponType);
-
-}
